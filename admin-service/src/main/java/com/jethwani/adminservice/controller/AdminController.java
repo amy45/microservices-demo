3e96aa7b0572cc6f8cca9b359dcb039f6f0e1b1a
Amit Jethwani <jethwaniamy45@gmail.com>
2021-02-07 03:54:40 +0530

    Admin Eureka

---
+++ admin-service/src/main/java/com/jethwani/adminservice/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.jethwani.adminservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+
+    @GetMapping
+    public String getAdmin(){
+        return "Admin Controller";
+    }
+}
